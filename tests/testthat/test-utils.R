54fae91c20c75a5bdc7f7dc90dceeb45f69e94e4
Juraj Szitas <szitas.juraj13@gmail.com>
2021-03-29 23:20:36 +0200

    Add test for run_iterativelly

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -18,3 +18,26 @@ test_that("Finding function arguments works", {
                  a = "call"
                ))
 })
+
+test_that("Running a function iteratively works", {
+
+  some_fun <-
+    function(x = 1,
+             y = 2) {
+      y <- paste0("a",y)
+      x <- x+y
+      return(x)
+    }
+
+  result <- run_iterativelly(some_fun, list( x = 1, y = 2 ))
+
+  expect_equal( length(result), 4 )
+  expect_equal( names(result), c("succesful",
+                                 "last_line",
+                                 "last_line_number",
+                                 "objects_in_scope") )
+  expect_equal( result[["succesful"]], FALSE )
+  expect_equal( result[["last_line"]], "x <- x + y")
+  expect_equal( result[["last_line_number"]], 3)
+  expect_equal( result[["objects_in_scope"]], list( x = 1, y = "a2"))
+})
